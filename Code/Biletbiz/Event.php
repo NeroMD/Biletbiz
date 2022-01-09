bfe2096bf1f90787ae260ed99a7756530a8f978d
Keremhan <keremhanmd@outlook.com>
2022-01-09 14:34:02 +0300

    All methods at their final version

--- Code/Biletbiz/Event.php
+++ Code/Biletbiz/Event.php
@@ -1,6 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About The Event</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            background-image: url(back.jpg);
+            background-size: 100% 100%;
+            height: 20cm;
+        }
+
+        .container .navbar {
+            width: 100%;
+            height: 100px;
+            background-color: #8b2323;
+        }
+
+        .navbar ul {
+            float: left;
+            margin-right: 20px;
+        }
+
+        .navbar .logo {
+            display: inline-block;
+            margin-left: 550px;
+            margin-top: 20px;
+            width: 10px;
+            height: 70px;
+        }
+
+        .footer {
+            background: #634a4a;
+            height: 180px;
+        }
+
+        .footertext {
+            font-size: 20px;
+            text-align: center;
+            line-height: 180px;
+        }
+
+        .part {
+            border: 1px solid black;
+            border-radius: 5px;
+            width: 600px;
+            height: 400px;
+            margin-top: 20px;
+        }
+
+        .part1 {
+
+            width: 500px;
+            height: 300px;
+
+        }
+
+        #people.adet {
+            margin-top: 10px;
+            border: 1px solid #b1b1b1;
+            height: 40px;
+            background: #5c3737;
+            font-size: 14px;
+            border-radius: 9px;
+            font-weight: bold;
+            color: #ffffff;
+
+        }
+
+        button {
+            background-color: #5c3737;
+            color: white;
+            padding: 9px 20px;
+            margin-left: 60px;
+            border-radius: 5px;
+            cursor: pointer;
+            width: 15%;
+            opacity: 0.9;
+        }
+
+        button:hover {
+            opacity: 1;
+        }
+    </style>
+</head>
 <?php
 include_once 'header.php';
+?>
+<body>
+    <div class="container">
+        <br><br>
+        <h2>
+            <center>About The Event</center>
+        </h2><br>
+        <center>
+        <?php
+
 
 if(isset($_GET["ID"])){
     
@@ -8,36 +110,48 @@ if(isset($_GET["ID"])){
     require_once 'includes/Event.inc.php';
     $row = EventDetail($conn, $_GET["ID"]);
     $remain= remainingCapacity($conn, $_GET["ID"]);
+    echo '<div class="part"><br><br>
+                <div class="part1">
+                    <h4>'.$row['EventName'].'</h4><br>
+                    <h5>Yer : '.$row['EventLocation'].'</h5>
+                    <h5>Tarih: '.$row['EventDate'].'</h5>
+                    <h5>Bilet : 150TL</h5><br>
+                    <p>'.$row['EventDescription'].'</p>
+                </div>';
 
 
 
-echo $row['EventName'].'<br>';
-echo $row['EventDescription'].'<br>';
-echo $row['EventLocation'].'<br>';
-echo $row['EventDate'].'<br>';
-echo $row['EventCapacity'].'<br>';
-echo $remain.'<br>';
 
 }
 if(isset($_SESSION["uid"])){
-    echo '<a href=bookingseat.php?ID='.$row["idEvent"].'>Buy ticket</a>';
+    
+    echo '<form action="bookingseat.php" method="post">
+        <select id="people" class="adet" name="AA" required>
+                    <option value="" disabled selected>Number Of Tickets</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+                <input type="hidden" name="ID" value="'.$row["idEvent"].'">
+                <button type="submit" name="submit">Buy</button>';
 }
 else{
     echo '<p>yok</p>';
 }
 ?>
+        
 
+                
+            </div>
+        </center>
+    </div>
+    </div>
+    <div class="footer">
+        <p class="footertext">Copyright &copy; BiletBiz 2021</p>
+    </div>
 
+</body>
 
-
-
-
-
-
-
-
-
-
-<?php
-    include_once 'footer.php'
-    ?>
+</html>
---
+++ Code/Biletbiz/myticket.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Tickets</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            background-image: url(back.jpg);
+            background-size: 100% 100%;
+            height: 20cm;
+        }
+
+        .container .navbar {
+            width: 100%;
+            height: 100px;
+            background-color: #8b2323;
+        }
+
+        .navbar ul {
+            float: left;
+            margin-right: 20px;
+        }
+
+        .navbar .logo {
+            display: inline-block;
+            margin-left: 550px;
+            margin-top: 20px;
+            width: 10px;
+            height: 70px;
+        }
+
+        .footer {
+            background: #634a4a;
+            height: 180px;
+        }
+
+        .footertext {
+            font-size: 20px;
+            text-align: center;
+            line-height: 180px;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 60%;
+            margin:auto;
+            margin-top: 50px;
+        }
+
+        th,
+        td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <a href="personalpage.html"><img src="logo.png" alt="Logo" style="width:250px; height:70px;"></a>
+            </div>
+        </div>
+        <br><br>
+        <h2><center>My Tickets</center></h2><br>
+        <table>
+            <tr>
+                <th>Event Name</th>
+                <th>Date</th>
+                
+                
+                <th>Seats</th>
+            </tr>
+            <?php
+            session_start();
+if(isset($_SESSION["uid"])){
+    
+    require_once 'includes/dbh.inc.php';
+            
+            $sql = "SELECT * FROM ticket where TUserEmail=? ;";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt,$sql)) {
+            echo "STMT FAIL";
+            
+             }   
+    
+            mysqli_stmt_bind_param($stmt,"s",$_SESSION['uid']);
+            mysqli_stmt_execute($stmt);
+    
+            $resultData = mysqli_stmt_get_result($stmt);
+            while ($row = mysqli_fetch_assoc($resultData)) {
+                $sql = "SELECT * FROM event where idEvent=? ;";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt,$sql)) {
+            echo "STMT FAIL";
+            
+             }   
+    
+            mysqli_stmt_bind_param($stmt,"s",$row["idEventID"]);
+            mysqli_stmt_execute($stmt);
+            $res=mysqli_stmt_get_result($stmt);
+            $ro=mysqli_fetch_assoc($res);
+            $date=$ro["EventDate"];
+    
+            $result=true;
+            $time = date("Y-m-d");
+            if ($date>$time) {
+            $result=false;
+            }
+                echo '<tr>
+                <td> '.$ro["EventName"].' </td>
+                <td> '.$ro["EventDate"].'</td>
+                
+                
+                
+                <td> '.$row["seat"].'</td>
+            </tr>';
+                if($result==false){echo '<form action="includes/refund.inc.php" method="post">
+                    <input type="hidden" name="ticket" value="'.$row['TicketID'].'">
+    <button type="submit" name="submit">refund</button>
+</form>';}
+                echo '<br>';
+            }
+            
+
+            mysqli_stmt_close($stmt);
+
+        
+        
+        
+        
+    
+    
+    
+    
+    
+}
+?>
+            <tr>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+            </tr>
+          
+        </table>
+    </div>
+    </div>
+    <div class="footer">
+        <p class="footertext">Copyright &copy; BiletBiz 2021</p>
+    </div>
+
+</body>
+
+</html>
