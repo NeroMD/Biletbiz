6432477c19bcfce9bd8feb7685975cd8cc1deb8d
Bekir Malik Özkuran <bekir.ozkuran@isik.edu.tr>
2022-01-09 21:02:32 +0300

    Add files via upload

---
+++ Biletbiz/header.php
@@ -0,0 +1,95 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+    <link rel="stylesheet" href="css/navbar.css">
+    <script defer src="http://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+<body>
+    
+    <div class="navbar" style="background-color: #8b2323;">
+        <ul>
+          <li><a href="#">Concert</a> </li>
+          <li><a href="#">Cinema</a> </li>
+          <li><a href="#">Theatre</a> </li>
+        </ul>
+        <form action="Search.php" method="post">
+        <div class="searchbox" method="post">
+          <input class="searchtxt" type="text" name="query" placeholder="Search...">
+          <input class="searchbutton" type="submit" methods="post" href="Search.php"><i class="fas fa-search"></i></input>
+        </div>
+        </form>
+        <div class="logo">
+            <a href="index.php"><img src="foto/logo.png" alt="Logo" style="width:250px; height:70px;"></a>
+        </div>
+        <?php
+        if(isset($_SESSION["uid"])){
+            echo  "<li><a href=''>profile</a></li>";
+            echo "<li><a href='includes/logout.inc.php'>Log out</a></li>";
+            if($_SESSION["isCompany"]==1){
+                echo "<li><a href='createEvent.php'>Create Event</a></li>";
+            }
+            if($_SESSION["isAdmin"]==1){
+                echo "<li><a href='BanUser.php'>BanUser</a></li><li><a href='Approve.php'>Company Request</a></li>";
+            }
+        }
+        else{
+            echo  '<!--Logn-->
+        <div class="login">
+          <ul class="login">
+            <a class="button" onclick="popupToggle();">Login</a>
+            <div id="popup">
+              <div class="content">
+                <img src="foto/user.png">
+                <section class="signinForm">    
+                    <form action="includes/login.inc.php" method="post">
+
+                          <div class="inputbox">
+                            <input type="text" placeholder="Enter Email Adress" name="email" required>
+                          </div>
+                          <div class="inputbox">
+                            <input type="password" placeholder="Enter Password" name="psswrd" required>
+                          </div>
+                          <div class="inputbox">
+                              <button class="popbuton" name="submit" type="submit" >Login</button>
+                          </div>
+                        </div>
+                        <a class="close" onclick="popupToggle();"><img src="foto/exit.png" style="width: 15px;height: 15px;"></a>
+                        </div>
+                  
+                    </form>
+                    <form action="ForgotPassword.php" method="post">
+            <div>
+            <input type="submit" title="Forgot Password" class="ForgotPassword" name="ForgotPassword"  method="POST" href="ForgotPassword.php?>">Forgot Password</input>
+            </div>
+        </form>
+            
+            </section>
+            
+            <script>
+              function popupToggle() {
+                const popup = document.getElementById("popup");
+                popup.classList.toggle("active")
+              }
+            </script>
+          </ul>
+        </div>
+        <div class="login">
+          <ul class="login">
+            <a href="signup.php" class="btn btn-warning">Register</a>
+          </ul>
+        </div>';
+           
+        }
+    ?>
+
+      </div>
+   
+</body>
---
+++ Biletbiz/signup.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="css/registercss.css">
+    <script defer src="http://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <a href="index.php"><img src="foto/logo.png" alt="Logo" style="width:250px; height:70px;"></a>
+            </div>
+            <div class="registerform">
+                <section class="signupForm">
+                    <form action="includes/signup.inc.php" method="post">
+                        <br>
+                        <h1>Register</h1>
+                        <p>Please fill in this form to create an account.</p>
+                        <hr>
+                        <label for="name"><b>Name & Surname</b></label>
+                        <input type="text" placeholder="Enter Name & Surname" name="name" required>
+
+                        <label for="email"><b>Email</b></label>
+                        <input type="email" placeholder="Enter Email" name="email" required>
+
+                        <label for="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="psswrd" required>
+
+                        <label for="psw-repeat"><b>Repeat Password</b></label>
+                        <input type="password" placeholder="Repeat Password" name="repsswrd" required>
+
+                        <label>
+                        <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+                        </label>
+                        <div class="clear">
+                            <button type="submit" name="submit" class="registerbutton">Register</button></a>
+                        <a href="index.php"><button type="button" class="cancelbutton">Cancel</button></a>
+                    
+                        </div>
+                    </form>
+                    <?php
+
+            if (isset($_GET["error"])){
+                if($_GET["error"] == 'emptyinput'){
+                    echo "<p>Fill all the input boxes</p>";
+                }
+                else if($_GET["error"] == 'invalidEmail'){
+                    echo "<p>Invalid Email</p>";
+                }
+                else if($_GET["error"] == 'pswrdNoMatch'){
+                    echo "<p>Password is not equal to repeat password</p>";
+                }
+                else if($_GET["error"] == 'emailInUse'){
+                    echo "<p>Email in use</p>";#not good for security
+                }
+                else if($_GET["error"] == 'stmtFail'){
+                    echo "<p>Something went wrong</p>";
+                }
+                else if($_GET["error"] == 'none'){
+                    echo "<p>Signup successful</p>";
+                }
+            }
+
+
+            ?>
+                </section>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
